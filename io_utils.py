590ee5ddefa37283e27ab9338a0dbeb659bd2a14
Hapereira04 <hapereira2004@gmail.com>
2026-07-05 21:36:10 +0100

    criação do esqueleto do módulo de leitura de dados da malha Adição do ficheiro io_utils.py com a estrutura base para o pré-processamento. Foram definidas as assinaturas e as docstrings das funções 'ler_nos_e_condicoes' e 'ler_elementos_finais', e importada a biblioteca NumPy. Este esqueleto prepara o sistema para a futura leitura de coordenadas geométricas, conectividade dos tetraedros e extração das condições de fronteira (Dirichlet).

---
+++ io_utils.py
@@ -0,0 +1,18 @@
+"""
+io_utils.py
+===========
+Módulo de entrada e saída (I/O) de dados para o simulador de Elementos Finitos (MEF).
+"""
+import numpy as np
+
+def ler_nos_e_condicoes(caminho_ficheiro):
+    """
+    Lê as coordenadas dos nós e as condições de fronteira (voltagens conhecidas).
+    """
+    pass
+
+def ler_elementos_finais(caminho_ficheiro):
+    """
+    Lê a conectividade dos tetraedros e a condutividade do material.
+    """
+    pass

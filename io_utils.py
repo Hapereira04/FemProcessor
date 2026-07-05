9053f42e49049a25c510db7a446f837d16443289
Hapereira04 <hapereira2004@gmail.com>
2026-07-06 00:14:24 +0100

    adicao do mapeamento de condicoes de fronteira Extensao da leitura de nos para inspecionar a 4a coluna. Implementado um dicionario que armazena o indice do no e a respetiva voltagem (Dirichlet) ignorando valores -1.0 (nos livres).

--- io_utils.py
+++ io_utils.py
@@ -7,23 +7,29 @@ import numpy as np
 
 def ler_nos_e_condicoes(caminho_ficheiro):
     """
-    Lê as coordenadas dos nós da malha.
+    Lê as coordenadas dos nós e as condições de fronteira (voltagens conhecidas).
     """
     Matriz_Nos = []
+    Condicoes_Fronteira = {}
 
     with open(caminho_ficheiro, 'r') as ficheiro_aberto:
         for linha_texto in ficheiro_aberto:
             linha_texto = linha_texto.strip()
 
-            # Ignorar linhas vazias ou comentários (que começam por #)
             if linha_texto and not linha_texto.startswith('#'):
                 valores_linha = list(map(float, linha_texto.split()))
 
                 if len(valores_linha) == 4:
-                    # Extrair apenas as 3 primeiras colunas (coordenadas x, y, z)
                     Matriz_Nos.append(valores_linha[:3])
 
-    return np.array(Matriz_Nos)
+                    indice_do_no = len(Matriz_Nos) - 1
+                    valor_potencial = valores_linha[3]
+
+                    # Se for diferente de -1.0, é uma Condição de Dirichlet (voltagem fixa)
+                    if valor_potencial != -1.0:
+                        Condicoes_Fronteira[indice_do_no] = valor_potencial
+
+    return np.array(Matriz_Nos), Condicoes_Fronteira
 
 def ler_elementos_finais(caminho_ficheiro):
     """

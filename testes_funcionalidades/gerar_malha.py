f7494b0b5c8f57558556ebb9e5f07ba0e0e9e2c1
Hapereira04 <hapereira2004@gmail.com>
2026-07-14 23:21:33 +0100

    Adicionar criação da geometria (terra e vara)
    Adiciona a construção da caixa de terra (bloco) e do cilindro da vara.
    Aplica a operação de corte para remover o volume da vara da terra.
    Sincroniza a geometria com o modelo.

--- testes_funcionalidades/gerar_malha.py
+++ testes_funcionalidades/gerar_malha.py
@@ -1,3 +1,5 @@
+from cmath import nan
+
 import gmsh
 import numpy as np
 import os
@@ -12,11 +14,32 @@ def gerar_malha_avancada(pasta="."):
     prof_terra = 40.0
     V_vara = 1000000.0
     Condutividade_Terra = 0.001
+    dist_varas = nan
 
     print("A iniciar o motor Gmsh...")
     gmsh.initialize()
     gmsh.option.setNumber("General.Terminal", 0)
     gmsh.model.add("Malha_Vara_Terra")
+
+    # 2. CONSTRUÇÃO DA GEOMETRIA
+    terra = gmsh.model.occ.addBox(-L_terra / 2, -L_terra / 2, -prof_terra, L_terra, L_terra, prof_terra)
+
+    varas = []
+    raio = diam_vara / 2.0
+    margem = 0.1
+
+    if num_varas == 1:
+        v1 = gmsh.model.occ.addCylinder(0, 0, margem, 0, 0, -comp_vara - margem, raio)
+        varas.append((3, v1))
+    else:
+        v1 = gmsh.model.occ.addCylinder(-dist_varas / 2, 0, margem, 0, 0, -comp_vara - margem, raio)
+        v2 = gmsh.model.occ.addCylinder(dist_varas / 2, 0, margem, 0, 0, -comp_vara - margem, raio)
+        varas.append((3, v1))
+        varas.append((3, v2))
+
+    gmsh.model.occ.cut([(3, terra)], varas, removeObject=True, removeTool=True)
+    gmsh.model.occ.removeAllDuplicates()
+    gmsh.model.occ.synchronize()
 
 
 if __name__ == "__main__":
